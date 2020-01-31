3b4f14f43ea6e6af7355dce78a0476ed3f96682b
Cristian Arteaga <carteaga19@gmail.com>
2020-01-31 03:11:01 -0300

    organizando

--- utils/CommandOrquester.js
+++ utils/CommandOrquester.js
@@ -9,15 +9,11 @@ class CommandOrchestrator {
 
   async execute(config) {
     const { command } = config;
-    let data = '';
-    for(const cmd of this.commands) {
-      if(cmd.command.toUpperCase() == command.toUpperCase()) {
+    for (const cmd of this.commands) {
+      if (cmd.command.toUpperCase() == command.toUpperCase()) {
         await cmd.execute(config);
-        console.log('obtuvo esta data', data);
       }
     }
-    console.log(data);
-    //return data;
   }
 }
 
