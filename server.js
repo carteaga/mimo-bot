cab0837d3dfd8f40d78c25dd77e323677a69933d
Cristian Arteaga <carteaga19@gmail.com>
2020-02-15 00:39:36 -0300

    mini server

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const sulla = require("sulla-hotfix");
 const CommandParser = require("./src/CommandParser");
 const commandOrquester = require("./src/InitCommand");
 const commandParser = new CommandParser();
+const express = require('express')
+const port = process.env.PORT || 8080;
+
+const app = express()
+app.use(express.json())
 
 async function start(client) {
   
@@ -38,3 +43,11 @@ sulla.create('session',{
   .catch(e => {
     console.log('error', e);
   });
+
+  app.get('/', async (req, res) => {
+    return res.send('Is alive!')
+  })
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
