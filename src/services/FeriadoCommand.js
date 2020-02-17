690fe87da0e2e40222fa3b773c7b0244338a0c6e
Cristian Arteaga <carteaga19@gmail.com>
2020-02-16 23:42:25 -0300

    se agrega cuanto falta para le feriado

--- src/services/FeriadoCommand.js
+++ src/services/FeriadoCommand.js
@@ -13,9 +13,9 @@ class FeriadoCommand {
 
   isHolidayValid(date) {
     return function filter(holiday) {
-      const dateHoliday = moment(holiday.fecha); 
+      const dateHoliday = moment(holiday.fecha);
       return date <= dateHoliday;
-    }
+    };
   }
 
   async execute({ command, params, context, client }) {
@@ -23,7 +23,9 @@ class FeriadoCommand {
     const maxHolidays = 5;
     const { from } = context;
     const year = currentDate.year();
-    const response = await getUrl(`https://apis.digital.gob.cl/fl/feriados/${year}`);
+    const response = await getUrl(
+      `https://apis.digital.gob.cl/fl/feriados/${year}`
+    );
     let msg = "No puedo ver los feriados, lo siento.";
 
     if (response) {
@@ -31,19 +33,20 @@ class FeriadoCommand {
         .filter(this.isHolidayValid(currentDate))
         .slice(0, maxHolidays);
 
-      msg = `Próximos ${maxHolidays || ''} feriados\n`;
+      msg = `Próximos ${maxHolidays || ""} feriados\n`;
       holidayRemaining.forEach(holiday => {
         const { nombre, fecha, irrenunciable, tipo } = holiday;
-        const isReligious = tipo == 'Religioso' ? "Si" : "No";
+        const isReligious = tipo == "Religioso" ? "Si" : "No";
         const isRenunciable = irrenunciable == 1 ? "Si" : "No";
-        const date = moment(fecha).format('D-MMM (dd)');
+        const date = moment(fecha);
 
         msg += [
-          `*${date}* \t_Rel? ${isReligious}\tRen? ${isRenunciable}_`,
-          `"${nombre}"`,
-          '', 
-          ''
-        ].join('\n');
+          `*${date.format("D-MMM (dd)")}* \t_Rel? ${isReligious}\tRen? ${isRenunciable}_`,
+          `\`\`\`"${nombre}"\`\`\``,
+          `\`\`\`(faltan ${date.fromNow(true)})\`\`\``,
+          "",
+          ""
+        ].join("\n");
       });
     }
 
