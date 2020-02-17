8195c345e3beed04cbf9c03e0091e352df381ce2
Cristian Arteaga <carteaga19@gmail.com>
2020-02-16 21:38:33 -0300

    agrega comandos automaticamente

--- src/InitCommand.js
+++ src/InitCommand.js
@@ -1,24 +1,13 @@
 const CommandOrquester = require("./CommandOrquester");
-
-// services
-const HelloCommand = require("./services/HelloCommand");
-const RollCommand = require("./services/RollCommand");
-const CaracolaCommand = require("./services/CaracolaCommand");
-const PokemonCommand = require("./services/PokemonCommand");
-const ChuckPhrasesCommand = require("./services/ChuckPhrasesCommand");
-const HoroscopoCommand = require("./services/HoroscopoCommand");
-const WikipediaCommand = require("./services/WikipediaCommand");
-const IsAliveCommand = require("./services/IsAliveCommand");
-const TransantiagoCommand = require("./services/TransantiagoCommand");
-
 const commandOrquester = new CommandOrquester();
-commandOrquester.addCommand(new HelloCommand());
-commandOrquester.addCommand(new RollCommand());
-commandOrquester.addCommand(new CaracolaCommand());
-commandOrquester.addCommand(new ChuckPhrasesCommand());
-commandOrquester.addCommand(new PokemonCommand());
-commandOrquester.addCommand(new HoroscopoCommand());
-commandOrquester.addCommand(new WikipediaCommand());
-commandOrquester.addCommand(new IsAliveCommand());
-commandOrquester.addCommand(new TransantiagoCommand());
+const fs = require("fs");
+const debug = require("debug")("app:server");
+const normalizedPath = require("path").join(__dirname, "services");
+
+fs.readdirSync(normalizedPath).forEach(function(file) {
+  const command = require(`./services/${file}`);
+  commandOrquester.addCommand(new command());
+  debug(`Se ha cargado el comando ${file}`);
+});
+
 module.exports = commandOrquester;
