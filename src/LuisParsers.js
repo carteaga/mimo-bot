a8de3b64100031a6379b22172a41822c89b223a6
Cristian Arteaga <carteaga19@gmail.com>
2020-07-10 03:52:06 -0400

    adapcion para luis ai

--- src/LuisParsers.js
+++ src/LuisParsers.js
@@ -7,10 +7,13 @@ class LuisParser {
     const request = `${config.luisEndPoint}/apps/${config.luisAppId}?verbose=true&timezoneOffset=0&subscription-key=${config.luisKey}&q=${message}`;
     const response = await getUrl(request);
 
-    if (response) {
-      return this.getIntention(response);
+    if (!response) {
+      return message;
     }
-    return message;
+
+    const command = this.getIntention(response);
+    const sign = command[0] !== '!' ? '!' : '';
+    return `${sign}${command}`;
   }
 
   getIntention(response) {
