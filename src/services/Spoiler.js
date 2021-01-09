3c1d357d8d9e97c3a748912247245521e1792f94
Cristian Arteaga <carteaga19@gmail.com>
2021-01-09 05:45:27 -0300

    chore: se agrega espacio para hacer desparecer el mensaje en notificacion

--- src/services/Spoiler.js
+++ src/services/Spoiler.js
@@ -4,15 +4,16 @@ class Spoiler extends Service {
   constructor() {
     super();
     this.command = '!spoiler';
-    this.help = 'Genera un spoiler de un text';
+    this.help = 'Genera un spoiler de un text (functiona solo en android)';
   }
 
   async execute({ params, client, context }) {
     const { from } = context;
-    const message = params.join(' ');
+    const message = params.join('');
     const title = '[SPOILER]';
-    const separator = String.fromCharCode('0x200B').repeat(2575);
-    const spoilerMessage = `${title} ${separator} ${message}`;
+    const separator = '\r\n'.repeat(3);
+    const padding = '\u200B'.repeat(4000);
+    const spoilerMessage = `${title}${separator}${padding}${message}`;
 
     await client.sendText(from, spoilerMessage);
   }
