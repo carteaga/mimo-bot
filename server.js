018f2cdfe64a957d5e0bec3d840d192319760c75
Cristian Arteaga <carteaga19@gmail.com>
2022-09-10 03:50:28 -0300

    (feat) add server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const sulla = require('@open-wa/wa-automate');
 const debug = require('debug')('app:server');
+const express = require('express');
 const { configBot, config } = require('./src/config/index');
 const startBot = require('./src/startBot');
+
+const app = express();
 
 sulla
   .create({
@@ -10,3 +13,11 @@ sulla
   })
   .then((client) => startBot(client))
   .catch((e) => console.log('error', e));
+
+app.get('/', async (req, res) => {
+  return res.sendFile('./index.html', { root: __dirname });
+});
+
+app.listen(config.port, () => {
+  debug(`Example app listening on port ${config.port}!`);
+});
