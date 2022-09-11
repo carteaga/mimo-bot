7c74a1bcaa6a520925b2bd8b7c7e1efd12eab65d
Cristian Arteaga <carteaga19@gmail.com>
2022-09-10 21:37:33 -0300

    (feat) add happy thursday command

---
+++ src/services/HappyThursday.js
@@ -0,0 +1,20 @@
+const Service = require('../Service');
+
+const HAPPY_THUESDAY_GIF =
+  'https://media.giphy.com/media/5nS2CCNuGyg5w3Eevt/giphy.gif';
+
+class HappyThursday extends Service {
+  constructor() {
+    super();
+    this.command = '!jueves';
+    this.help = 'Feliz jueves!';
+  }
+
+  async execute({ context, client, params }) {
+    const { from } = context;
+
+    await client.sendGiphyAsSticker(from, HAPPY_THUESDAY_GIF);
+  }
+}
+
+module.exports = HappyThursday;
