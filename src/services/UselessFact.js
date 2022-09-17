f4034b3e0117b63dd2c38b9dad5a5848bee9fc2a
Cristian Arteaga <carteaga19@gmail.com>
2022-09-17 00:15:18 -0300

    (feat) Can send 3 fact on once time

--- src/services/UselessFact.js
+++ src/services/UselessFact.js
@@ -1,20 +1,41 @@
 const { getUrl } = require('../utils/getUrl');
 const Service = require('../Service');
+const { isNumber } = require('../utils/validations');
+
+const MAX_FACTS = 3;
+const FACT_URL = 'https://uselessfacts.jsph.pl//random.json?language=en';
 
 class UselessFact extends Service {
   constructor() {
     super();
     this.command = '!ufact';
-    this.help = 'Obtén algún dato inservible';
+    this.help = 'Obtén algún dato inservible. !ufact [cantidad]. 3 max.';
   }
 
-  async execute({ context, client }) {
-    const { from } = context;
-    const { text } = await getUrl(
-      'https://uselessfacts.jsph.pl//random.json?language=en'
-    );
+  async getFact() {
+    const { text } = await getUrl(FACT_URL);
+    return text;
+  }
 
+  async sendFact(client, from) {
+    const text = await this.getFact();
     text && client.sendText(from, text);
+  }
+
+  async execute({ context, client, params }) {
+    const { from } = context;
+    const [numberFact] = params;
+
+    if (!isNumber(numberFact)) {
+      this.sendFact(client, from);
+      return;
+    }
+
+    const facts = numberFact > MAX_FACTS ? MAX_FACTS : Number(numberFact);
+    const promises = Array(facts)
+      .fill(0)
+      .map((_) => this.sendFact(client, from));
+    Promise.all(promises);
   }
 }
 
---
+++ src/utils/validations.js
@@ -0,0 +1,5 @@
+const isNumber = (number) => !isNaN(number);
+
+module.exports = {
+  isNumber
+}
